c5e0f0947021db104081d7dc760cc3ba3a39e3b0
desoares <fabio.desoares@gmail.com>
2017-05-22 08:30:49 -0300

    add views

--- app/server/index.js
+++ app/server/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-const express = require('express');
+const express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app);
+
+app.set('view engine', 'pug');
+app.set('views', `${__base}app/views` );
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Todo application up and running on port ${process.env.PORT}`);
+});
